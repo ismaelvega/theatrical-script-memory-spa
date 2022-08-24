2eef058017c17b50282783db78c643eb9bbe5429
Ismael Vega <ismaelgrive@gmail.com>
2022-08-24 16:13:57 -0500

    yo why not working

--- pages/index.js
+++ pages/index.js
@@ -83,7 +83,7 @@ export default function Home({ shuffledData, rawData }) {
   );
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
   const port = process.env.PORT || 3000;
   const response = await fetch(`http://localhost:${port}/api/sentences`);
   const data = await response.json();
